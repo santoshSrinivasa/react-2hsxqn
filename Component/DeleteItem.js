c142e5ac3951137f1598a17043481a7ee650b082
santoshSrinivasa <santoshsrinivasa94@gmail.com>
2019-08-30 11:58:07 +0530

    1 file modified, 4 added, 1 deleted

---
+++ Component/DeleteItem.js
@@ -0,0 +1,29 @@
+import React,{Component} from 'react';
+export default class DeleteItem extends Component {
+  constructor(props)
+  {
+    super(props);
+  }
+  render()
+  {
+    return(
+        <li>
+            {this.props.text} <span onClick={()=> this.props.remove(this.props.text)}>X</span>
+        </li>
+    );
+  }
+}import React,{Component} from 'react';
+export default class DeleteItem extends Component {
+  constructor(props)
+  {
+    super(props);
+  }
+  render()
+  {
+    return(
+        <li>
+            {this.props.text} <span onClick={()=> this.props.remove(this.props.text)}>X</span>
+        </li>
+    );
+  }
+}
--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+	import React, { Component } from 'react';
+	import { render } from 'react-dom';
+	import './style.css';
+	import Todo from "./Component/Todo";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+	render(<Todo />, document.getElementById('root'));
